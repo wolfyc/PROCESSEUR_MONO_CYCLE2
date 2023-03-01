b2947e0c488fb7b9602324a4c0a03da65645a4be
wolfyc <zaidimedh@gmail.com>
2023-03-01 16:18:14 +0100

    mux2

--- mux2.vhd
+++ mux2.vhd
@@ -17,9 +17,8 @@ entity mux2 is
 
     Architecture behav of mux2 is
     begin
-
-
         with Com select 
-        s <= A when '0', B when '1',
-		(others => '0') when others ;
+            s <= A when '0', B when '1',
+            (others => '0') when others ;
+
     end architecture;
